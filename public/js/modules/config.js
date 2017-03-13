4f65f391cc09c038b3ed11309ebb071b2f5aa0ab
Brad Cupp <bradcupp@MacBook-Pro.local>
2017-03-13 10:45:16 -0400

    populated config file

---
+++ public/js/modules/config.js
@@ -0,0 +1,15 @@
+var app = angular.module('myMod');
+
+app.config(function($routeProvider, $locationProvider){
+  $routeProvider
+    .when('/', {
+      controller: '',
+      templateUrl: '.html'
+    })
+    .when('/', {
+      controller: '',
+      templateUrl: ''
+    });
+
+  $locationProvider.hashPrefix('');
+});
