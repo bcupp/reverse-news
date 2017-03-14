1c3a279a322c9da024464410b7072e367c6c2c87
Brad Cupp <bradcupp@MacBook-Pro.local>
2017-03-14 15:29:10 -0400

    removed lightbox info

--- public/js/controllers/controller.js
+++ public/js/controllers/controller.js
@@ -7,32 +7,21 @@ app.controller('controller1', function($scope, newService, $location) {
 
     newService.getNews().then(function(resultOfPromise) {
         newsFeed = resultOfPromise;
-
-        console.log($scope.displayedNewsPromise);
-
     });
     newService.getNewsAbcNewsAu().then(function(resultOfPromise) {
         newsFeed = newsFeed.concat(resultOfPromise);
-
-        console.log($scope.displayedNewsPromise);
-
     });
     newService.getNewsArsTech().then(function(resultOfPromise) {
         newsFeed = newsFeed.concat(resultOfPromise);
-
-
-        console.log($scope.displayedNewsPromise);
-      
     });
+
+    
     // emailjs.send("mailjet","template_Fj79lA9W",{name: "James", notes: "Check this out!"})
     // .then(function(response) {
     //    console.log("SUCCESS. status=%d, text=%s", response.status, response.text);
     // }, function(err) {
     //    console.log("FAILED. error=", err);
     // });
-    //
-    // $scope.displayedNews = newService.getNews();
-    // console.log($scope.displayedNews);
 
     function newsArray(userInput) {
         var reverseFilter = [];
@@ -52,6 +41,7 @@ app.controller('controller1', function($scope, newService, $location) {
             normFilter: normFilter
         };
         console.log($scope.news);
+        $("#id1").remove();
     };
 
     $scope.userSearchReverse = function(userInput) {
